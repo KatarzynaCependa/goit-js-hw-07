b26a4823a5cc890a04e4048ed64aa2cd068126f1
Katarzyna Cependa <kasia.cependa@gmail.com>
2023-03-29 20:23:39 +0200

    Adding script to task no. 1

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # goit-js-hw-07
+
+https://katarzynacependa.github.io/goit-js-hw-07/
--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,34 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+
+for (const item of galleryItems) {
+  galleryEl.insertAdjacentHTML(
+    "beforeend",
+    `<li class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</li>`
+  );
+}
+
+// galleryEl.addEventListener("click", (event) => {
+//   event.preventDefault();
+// });
+
+// galleryEl.onclick = () => {
+//   basicLightbox
+//     .create(
+//       `<img width="1400" height="900" src="https://placehold.it/1400x900">`
+//     )
+//     .show();
+// };
